59903703f203ebce7d0a4a55443b93b13be00664
miura K <kanae@TomoenoMacBook-puro.local>
2020-05-20 23:17:01 +0900

    未解決

--- src/pages/Lesson07/index.js
+++ src/pages/Lesson07/index.js
@@ -5,23 +5,27 @@ import instruction from "./instruction.md";
 
 const convertData = (input) => {
   //各プロパティcreatedAtの値を時間以外のみに変える
-  //inputオブジェクトのプロパティを変数itemに代入して処理を繰り返す
+  //各データ(input）を変数itemに代入して処理を繰り返す
   for (const item of input) {
-    const d = new Date(`${item.createdAt} UTC`); //値をまず取得
+    //値をまず取得 
+    //new Data()の返り値はUTC (←この１行で−9時間になっているのか？)
+    const d = new Date(`${item.createdAt} UTC`); 
     const year = d.getFullYear();
+    //padStartメゾットは配列に対して使用するため、文字列に変換する
     const month = `${d.getMonth() + 1}`.padStart(2, "0"); //1月＝０となるため+1
     const date = `${d.getDate()}`.padStart(2, "0");
-    //padStartメゾットは配列に対して使用するため、文字列に変換する
     item.createdAt = `${year}-${month}-${date}`;
   }
-  //createdAtの要素のみの配列を作る？
+  //各createdAtプロパティの値のみ集めた配列を作って昇順に（重複なし）
   const datas = Array.from(new Set(input.map(({createdAt}) => createdAt)));
   datas.sort(); //文字列の場合は引数なしで大丈夫
+  //countオブジェを生成 (←なぜ？)
   const count = {
     tweet:{},
     retweet:{}
   };
-  //各件数の初期化
+  /*配列datasの各要素に件数のプロパティを追加・初期化
+  つまり各日付の各件数をそれぞれ初期化*/ 
   for(const d of datas){
     count.tweet[d] = 0;
     count.retweet[d] = 0;
@@ -34,7 +38,8 @@ const convertData = (input) => {
       count.tweet[createdAt] += 1;
     }
   }
-  //オブジェクト内を作成
+  /*目的の形になるようなオブジェクトに変形する
+  まず配列０番目の要素tweet、次に１番目の要素vretweerを処理している*/
   return ["tweet","retweet"].map((item) =>{
     return {
       id: item,
