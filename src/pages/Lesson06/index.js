ad8efcd662cd0d42e4f93e7e6753138a9f6a23db
miura K <kanae@TomoenoMacBook-puro.local>
2020-05-20 23:28:23 +0900

    メモ付き

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -4,12 +4,13 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  //男性=blue、女性=redとなるcolorプロパティに使う
+  //男性=blue、女性=redとなるcolorプロパティ
   const colors = {
     男性: "blue",
     女性: "red",
   };
   //欲しい引数のみ貰って、各データを目的のオブジェクトに変更
+  //mapメゾット：配列の要素を順番にコールバック関数へ渡し、新たな配列を作る
   return input.map(({gender,x,y}) => {
     return {
       //貰った引数genderと一致しているcolorsオブジェクトのプロパティ名で設定
