d781d6450ac333bf52e765ec4f9ece89813d7b98
miura K <kanae@TomoenoMacBook-puro.local>
2020-05-20 20:03:34 +0900

    メモ付き

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,7 +4,12 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  input.sort((a,b) => (b.count-a.count));
+  //各プロパティcountの値をソートする
+  //２つの値を比較しながら１つずつ順番を入れ替える手法（比較関数）
+  //曻順：a-b、降順：b-a 
+  input.sort(function(a,b){
+    return b.count-a.count;
+  });
   return input.slice(0,20);
 };
 
