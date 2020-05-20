2f88b549459275fdb975073e7c811b052ac5d588
miura K <kanae@TomoenoMacBook-puro.local>
2020-05-20 19:48:59 +0900

    メモ付き

--- src/pages/Lesson01/index.js
+++ src/pages/Lesson01/index.js
@@ -4,9 +4,11 @@ import Chart from "../../components/Chart01";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return input.map(([name,count]) => (
-    {name,count}
-  ));
+  //エントリーの配列から連想配列（オブジェクト）へ変換
+  //mapメゾット：配列の要素を順番にコールバック関数へ渡し、新たな配列を作る
+  return input.map(function([name,count]){
+    return ({name,count});
+  });
 };
 
 const Lesson = () => {
