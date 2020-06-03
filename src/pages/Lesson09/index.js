e7782df85f08bc7a7351680ff2f81ebf0c8c12a7
miura K <kanae@TomoenoMacBook-puro.local>
2020-06-03 23:03:48 +0900

    考え中

--- src/pages/Lesson09/index.js
+++ src/pages/Lesson09/index.js
@@ -4,7 +4,22 @@ import Chart from "../../components/Chart09";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return { children: [] }; // ここを作りましょう！
+  /*
+  1:各省（ministry）と一致するデータを集める（グループ化）
+  2:各省内で部局（bureau）を集める
+  3:各部局内で担当課（department）を集める
+  4:
+  */
+
+  //まずは省がいくつあるのかを知ってから各省ごとにデータ集める
+  const mTipe = Array.from(new Set(input.map(({ministry}) => ministry)))
+    .map((m) => {
+     const ministrys = input.filter((item => item.ministry === m)); //1
+
+  })
+  
+
+  //return { children: [] }; // ここを作りましょう！
 };
 
 const Lesson = () => {
